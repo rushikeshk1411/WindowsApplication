21642a472aaec1d9bff4a63b20c34084ddf1e6dd
Rushikesh Bhausaheb Khaire <130335306+rushikeshk1411@users.noreply.github.com>
2023-09-11 20:15:26 +0530

    Final Project NIT Commit

---
+++ Employee Info App/EmployeeInfo.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Configure
+{
+    partial class EmployeeInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmployeeInfo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbEname = new System.Windows.Forms.TextBox();
+            this.tbJob = new System.Windows.Forms.TextBox();
+            this.tbSalary = new System.Windows.Forms.TextBox();
+            this.tbEno = new System.Windows.Forms.TextBox();
+            this.lblalary = new System.Windows.Forms.Label();
+            this.lblJob = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClearImage = new System.Windows.Forms.Button();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(636, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee Details";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbEname
+            // 
+            this.tbEname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEname.Location = new System.Drawing.Point(112, 89);
+            this.tbEname.Name = "tbEname";
+            this.tbEname.Size = new System.Drawing.Size(223, 29);
+            this.tbEname.TabIndex = 7;
+            // 
+            // tbJob
+            // 
+            this.tbJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbJob.Location = new System.Drawing.Point(112, 124);
+            this.tbJob.Name = "tbJob";
+            this.tbJob.Size = new System.Drawing.Size(223, 29);
+            this.tbJob.TabIndex = 9;
+            // 
+            // tbSalary
+            // 
+            this.tbSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSalary.Location = new System.Drawing.Point(112, 159);
+            this.tbSalary.Name = "tbSalary";
+            this.tbSalary.Size = new System.Drawing.Size(223, 29);
+            this.tbSalary.TabIndex = 11;
+            // 
+            // tbEno
+            // 
+            this.tbEno.Enabled = false;
+            this.tbEno.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEno.Location = new System.Drawing.Point(112, 52);
+            this.tbEno.Name = "tbEno";
+            this.tbEno.Size = new System.Drawing.Size(223, 29);
+            this.tbEno.TabIndex = 14;
+            // 
+            // lblalary
+            // 
+            this.lblalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblalary.Location = new System.Drawing.Point(10, 162);
+            this.lblalary.Name = "lblalary";
+            this.lblalary.Size = new System.Drawing.Size(96, 25);
+            this.lblalary.TabIndex = 13;
+            this.lblalary.Text = "Salary:";
+            // 
+            // lblJob
+            // 
+            this.lblJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJob.Location = new System.Drawing.Point(10, 127);
+            this.lblJob.Name = "lblJob";
+            this.lblJob.Size = new System.Drawing.Size(65, 25);
+            this.lblJob.TabIndex = 12;
+            this.lblJob.Text = "Job:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ename:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Eno:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(123, 203);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(103, 48);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(14, 203);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(103, 48);
+            this.btnSelect.TabIndex = 15;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(232, 203);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(103, 48);
+            this.btnInsert.TabIndex = 17;
+            this.btnInsert.Text = "New";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(232, 257);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(103, 45);
+            this.btnClose.TabIndex = 20;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(123, 257);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(103, 45);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(14, 257);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(103, 45);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(341, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClearImage
+            // 
+            this.btnClearImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClearImage.BackgroundImage")));
+            this.btnClearImage.Location = new System.Drawing.Point(594, 52);
+            this.btnClearImage.Name = "btnClearImage";
+            this.btnClearImage.Size = new System.Drawing.Size(23, 23);
+            this.btnClearImage.TabIndex = 22;
+            this.btnClearImage.UseVisualStyleBackColor = true;
+            this.btnClearImage.Click += new System.EventHandler(this.btnClearImage_Click);
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadImage.Location = new System.Drawing.Point(341, 254);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(276, 45);
+            this.btnLoadImage.TabIndex = 23;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // EmployeeInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 313);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.btnClearImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.tbEname);
+            this.Controls.Add(this.tbJob);
+            this.Controls.Add(this.tbSalary);
+            this.Controls.Add(this.tbEno);
+            this.Controls.Add(this.lblalary);
+            this.Controls.Add(this.lblJob);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EmployeeInfo";
+            this.Text = "EmployeeInfo";
+            this.Load += new System.EventHandler(this.EmployeeInfo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbEname;
+        private System.Windows.Forms.TextBox tbJob;
+        private System.Windows.Forms.TextBox tbSalary;
+        private System.Windows.Forms.TextBox tbEno;
+        private System.Windows.Forms.Label lblalary;
+        private System.Windows.Forms.Label lblJob;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClearImage;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Employee Info App/EmployeeInfo.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.SqlTypes;
+using System.IO;
+using static Microsoft.VisualBasic.Interaction;
+using System.Linq.Expressions;
+
+namespace Configure
+{
+    public partial class EmployeeInfo : Form
+    {
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataReader dr;
+        string imgpath = "";
+        byte[] imgData = null;
+        public EmployeeInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void EmployeeInfo_Load(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["SqlCon"].ConnectionString;
+            con = new SqlConnection(str);
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string TEno = InputBox("Enter the Eno for search");
+                if (int.TryParse(TEno, out int Eno))
+                {
+                    try
+                    {
+                        cmd.CommandText = "NEMployee_Select";
+                        cmd.Parameters.Clear();
+                        cmd.Parameters.AddWithValue("Eno", Eno);
+                        cmd.Parameters.AddWithValue("Status", true);
+
+                        con.Open();
+                        dr = cmd.ExecuteReader();
+                        if (dr.Read())
+                        {
+                            tbEno.Text = dr["Eno"].ToString();
+                            tbEname.Text = dr["Ename"].ToString();
+                            tbJob.Text = dr["Job"].ToString();
+                            tbSalary.Text = dr["Salary"].ToString();
+
+                            if (dr["Photo"] != DBNull.Value)
+                            {
+                                imgData = (byte[])dr["Photo"];
+                                MemoryStream ms = new MemoryStream(imgData);
+                                pictureBox1.Image = Image.FromStream(ms);
+                            }
+                            else
+                            {
+                                imgData = null;
+                                imgpath = "";
+                                pictureBox1.Image = null;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("You are at last row of the table", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Enter the interger Number");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbEno.Text = tbEname.Text = tbJob.Text = tbSalary.Text = "";
+            pictureBox1.Image = null;
+            tbEname.Focus();
+            imgpath = "";
+            imgData = null;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {   
+            if (btnInsert.Text == "New")
+            {
+                btnClear.PerformClick();
+                btnInsert.Text = "Insert";
+            }
+            else
+            {
+                try
+                {
+                    cmd.Parameters.Clear();
+                    cmd.CommandText = "NEmployee_Insert";
+                    con.Open();
+                    //dr.Close();
+                    //cmd.Parameters.AddWithValue("@Eno", tbEno.Text);
+                    cmd.Parameters.Add("Eno", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.AddWithValue("Ename", tbEname.Text);
+                    cmd.Parameters.AddWithValue("Job", tbJob.Text);
+                    cmd.Parameters.AddWithValue("Salary", tbSalary.Text);
+
+                    if (imgpath.Trim().Length > 0)
+                    {
+                        imgData = File.ReadAllBytes(imgpath);
+                        cmd.Parameters.AddWithValue("Photo", imgData);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("Photo", DBNull.Value);
+                        cmd.Parameters["Photo"].SqlDbType = SqlDbType.VarBinary;
+                    }
+                    cmd.ExecuteNonQuery();
+                    tbEno.Text = cmd.Parameters["Eno"].Value.ToString();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    btnInsert.Text = "New";
+                    imgData = null;
+                    imgpath = "";
+                    con.Close();
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "NEmployee_Update";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("Eno", tbEno.Text);
+            cmd.Parameters.AddWithValue("Ename", tbEname.Text);
+            cmd.Parameters.AddWithValue("Job", tbJob.Text);
+            cmd.Parameters.AddWithValue("Salary", tbSalary.Text);
+
+            if(imgData == null && imgpath.Trim().Length == 0)
+            {
+                cmd.Parameters.AddWithValue("Photo", DBNull.Value);
+                cmd.Parameters["Photo"].SqlDbType = SqlDbType.VarBinary;
+            }
+            else if(imgpath.Trim().Length > 0)
+            {
+                imgData= File.ReadAllBytes(imgpath);
+                cmd.Parameters.AddWithValue("Photo", imgData);
+                //MessageBox.Show(imgData.ToString());
+            }
+            else if(imgpath.Trim().Length == 0 && imgpath != null)
+            {
+                cmd.Parameters.AddWithValue("Photo", imgData);
+            }
+            con.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Data Updation is succesful", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are you want to Sure to Deleting ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(dr == DialogResult.Yes)
+            {
+                cmd.Parameters.Clear();
+                con.Open();
+                cmd.CommandText = "NEmployee_Delete";
+                cmd.Parameters.AddWithValue("Eno", tbEno.Text);
+                cmd.ExecuteNonQuery();
+                btnClear.PerformClick();
+                con.Close();
+                MessageBox.Show("Record is successful deleted", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            this.Close();
+        }
+
+        private void btnClearImage_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            imgpath = "";
+            imgData = null;
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Jpg Images|*.jpg|Jpeg Images|*.jpeg|ALL|*.*";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                imgpath = openFileDialog1.FileName;
+                pictureBox1.ImageLocation = imgpath;
+            }
+        }
+    }
+}
